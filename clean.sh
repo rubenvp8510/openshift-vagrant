a6f6ede46d32e1a4cd4aa07df06df8e51228811f
Ruben Vargas <ruben.vp8510@gmail.com>
2017-07-10 19:19:26 -0500

    Improve clean script. change openshift-ansible version

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+vm_uuid=$(cat .vagrant/machines/default/virtualbox/id)
+disk_uuid=$(vboxmanage showvminfo  ${vm_uuid} | grep second_hdd | sed -n 's/.* (UUID: \(.*\))/\1/p' )
 vagrant destroy -f
+vboxmanage closemedium disk ${disk_uuid}  --delete
 rm -Rf ./dev/
-
--- clone-ansible.sh
+++ clone-ansible.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /opt/ansible
-git clone https://github.com/rubenvp8510/openshift-ansible.git . -b hawkular-pv-logs
+git clone https://github.com/rubenvp8510/openshift-ansible.git . -b openshift-ansible-3.5.93-1
